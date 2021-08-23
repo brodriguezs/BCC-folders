c7e6f6850d3c8d214da4496ba01fd74a6636d35c
Bryan Rodriguez Siatama <rodriguezbryan03@gmail.com>
2021-08-23 10:46:59 -0500

    chore: initial setup and dependencies

---
+++ src/Folder.js
@@ -0,0 +1,28 @@
+export class Folder {
+    constructor() {}
+
+    /**
+     * TODO This will CREATE a folder
+     * @param {string} name folders name
+     */
+    create(name){}
+
+    /**
+     * TODO This will MOVE a folder
+     * @param {string} path folder path to move
+     * @param {string} to destination path
+     */
+    move(path, to){}
+
+    /**
+     * TODO This will LIST a folder
+     * @return {string} directory tree
+     */
+    list(){}
+
+    /**
+     * TODO This will DELETE a folder
+     * @param {string} folderName
+     */
+    delete(folderName){}
+}
